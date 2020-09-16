baf26bf2b9dbe9989c2d8cbd0a27658879263ec6
Mika Nevalainen <mn.nevalainen@gmail.com>
2020-09-16 16:10:08 +0300

    Resolve target-data on target page

--- src/target/page/TargetPage.tsx
+++ src/target/page/TargetPage.tsx
@@ -1,5 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import apiRequest, { Target } from '../../common/utils/api-request';
 
 export default function TargetPage({ id }: { id: string }): JSX.Element {
-  return <div className="target">Kohdesivu {id}</div>;
+  const [target, setTarget] = useState<Target | null>(null);
+
+  useEffect(() => {
+    apiRequest.getTarget(id).then(setTarget);
+  }, [id]);
+
+  return <div className="target">{target && target.name}</div>;
 }
