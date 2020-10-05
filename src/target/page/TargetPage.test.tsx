ced6c27f45e731b094f6b929fea5a2940765e311
Mika Nevalainen <mn.nevalainen@gmail.com>
2020-10-05 17:21:46 +0300

    Add tests for target-page states

--- src/target/page/TargetPage.test.tsx
+++ src/target/page/TargetPage.test.tsx
@@ -1,36 +1,71 @@
 import React from 'react';
-import { mount, ReactWrapper, shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import { act } from 'react-dom/test-utils';
-import api from '../../common/utils/api/api';
+import api, { Target } from '../../common/utils/api/api';
 import TargetPage from './TargetPage';
 
-jest.mock('../../common/utils/api/api');
-const apiMock = api as jest.Mocked<typeof api>;
-jest.spyOn(React, 'useEffect').mockImplementation((f) => f());
+const TEST_TARGET: Target = {
+  id: 'tprek:1000',
+  name: 'Test target',
+  address: 'Main street, Helsinki',
+  description: 'Test target description',
+};
 
-describe.only(`<TargetPage />`, () => {
-  test('should show loading indicator', () => {
-    const targetPage = shallow(<TargetPage id="tprek:8100" />);
+describe(`<TargetPage />`, () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('should show loading indicator', async () => {
+    jest.spyOn(api, 'getTarget').mockImplementation(() =>
+      // eslint-disable-next-line prefer-promise-reject-errors
+      Promise.resolve(TEST_TARGET)
+    );
+
+    const targetPage = mount(<TargetPage id="tprek:8100" />);
+
+    await act(async () => {
+      targetPage.update(); // First tick to trigger useEffect to load
+    });
+
     expect(targetPage.find('p').text()).toEqual(
       'Toimipisteen tietoja ladataan...'
     );
   });
 
-  test('should error notification', () => {
-    let targetPage: ReactWrapper | undefined;
-    apiMock.getTarget.mockResolvedValue(
+  test('should show error notification', async () => {
+    jest.spyOn(api, 'getTarget').mockImplementation(() =>
       // eslint-disable-next-line prefer-promise-reject-errors
-      Promise.reject('Can not load target')
+      Promise.reject('Failed to load a target')
     );
 
-    act(() => {
-      targetPage = mount(<TargetPage id="tprek:8100" />);
-      targetPage.setProps({});
-      console.log(targetPage?.html());
+    const targetPage = mount(<TargetPage id="tprek:8100" />);
+
+    await act(async () => {
+      targetPage.update(); // First tick for useEffect
     });
 
-    expect(targetPage?.find('p').text()).toEqual(
-      'Toimipisteen tietoja ei saatu ladattua....'
+    targetPage.update(); // Second tick for useState
+
+    expect(targetPage.text()).toContain('Toimipistettä ei saatu ladattua.');
+  });
+
+  test('should show target details', async () => {
+    jest.spyOn(api, 'getTarget').mockImplementation(() =>
+      // eslint-disable-next-line prefer-promise-reject-errors
+      Promise.resolve(TEST_TARGET)
     );
+
+    const targetPage = mount(<TargetPage id={TEST_TARGET.id} />);
+
+    await act(async () => {
+      targetPage.update(); // First tick for useEffect
+    });
+
+    targetPage.update(); // Second tick for useState
+
+    expect(targetPage.find('h1').text()).toEqual(TEST_TARGET.name);
+    expect(targetPage.find('address').text()).toEqual(TEST_TARGET.address);
+    expect(targetPage.find('p').text()).toEqual(TEST_TARGET.description);
   });
 });
