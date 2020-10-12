8a4480b0e29ac98f91c6612a864e9a22e2448cf1
Mika Nevalainen <mn.nevalainen@gmail.com>
2020-10-12 14:00:40 +0300

    Visit target-page in e2e-tests

--- cypress/integration/App.spec.ts
+++ cypress/integration/App.spec.ts
@@ -1,12 +1,15 @@
+/// <reference types="cypress" />
 /// <reference types="cypress-axe" />
 
 describe('Open aukiolot app', () => {
   beforeEach(() => {
-    cy.visit('/');
+    cy.visit(`/target/${Cypress.env('target-id')}`);
   });
+
   it('Contains correct page title', () => {
     cy.contains('Aukiolot');
   });
+
   it('Has no detectable a11y violations on load', () => {
     cy.injectAxe();
     cy.checkA11y();
