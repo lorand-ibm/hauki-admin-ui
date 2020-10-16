305a062137d4ef72446efb37cbf82e43e2f2d77c
Mika Nevalainen <mn.nevalainen@gmail.com>
2020-10-16 12:05:59 +0300

    Read authentication-parameters for context-provider from localStorage or url
    Reading from localStorage does not work yet

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useEffect } from 'react';
 import {
   BrowserRouter as Router,
   Route,
@@ -6,35 +6,60 @@ import {
   RouteComponentProps,
 } from 'react-router-dom';
 import 'hds-core';
+import querystring, { ParsedUrlQuery } from 'querystring';
+import {
+  Tokens,
+  setTokens,
+  getTokens,
+  convertParamsToTokens,
+  isValidAuthParams,
+  AuthContext,
+} from './auth/auth-context';
 import Main from './components/main/Main';
 import NavigationAndFooterWrapper from './components/navigation-and-footer-wrapper/NavigationAndFooterWrapper';
 import './App.scss';
 import TargetPage from './target/page/TargetPage';
 
 export default function App(): JSX.Element {
+  const existingTokens: Tokens | undefined = getTokens();
+
+  const parameterTokens: ParsedUrlQuery = querystring.parse(
+    window.location.search.replace('?', '')
+  );
+
+  const queryTokens = isValidAuthParams(parameterTokens)
+    ? convertParamsToTokens(parameterTokens)
+    : undefined;
+
+  useEffect(() => {
+    setTokens(existingTokens || queryTokens);
+  }, [existingTokens, queryTokens]);
+
   return (
     <div className="App">
-      <Router>
-        <NavigationAndFooterWrapper>
-          <Main id="main">
-            <Switch>
-              <Route exact path="/">
-                <h1>Etusivu</h1>
-              </Route>
-              <Route
-                id="target-route"
-                exact
-                path="/target/:id"
-                render={({
-                  match,
-                }: RouteComponentProps<{ id: string }>): ReactElement => (
-                  <TargetPage id={match.params.id} />
-                )}
-              />
-            </Switch>
-          </Main>
-        </NavigationAndFooterWrapper>
-      </Router>
+      <AuthContext.Provider value={{ tokens: queryTokens }}>
+        <Router>
+          <NavigationAndFooterWrapper>
+            <Main id="main">
+              <Switch>
+                <Route exact path="/">
+                  <h1>Etusivu</h1>
+                </Route>
+                <Route
+                  id="target-route"
+                  exact
+                  path="/target/:id"
+                  render={({
+                    match,
+                  }: RouteComponentProps<{ id: string }>): ReactElement => (
+                    <TargetPage id={match.params.id} />
+                  )}
+                />
+              </Switch>
+            </Main>
+          </NavigationAndFooterWrapper>
+        </Router>
+      </AuthContext.Provider>
     </div>
   );
 }
