9ebf77c568df06ff1d4b329182e17fbe3ab33c83
Mika Nevalainen <mn.nevalainen@gmail.com>
2020-10-28 14:07:08 +0200

    Parse username from params and use it in the assert

--- cypress/integration/User.spec.ts
+++ cypress/integration/User.spec.ts
@@ -1,14 +1,18 @@
 /// <reference types="cypress" />
 /// <reference types="node"/>
-import querystring from 'querystring';
+/// <reference path="../index.d.ts" />
+import * as querystring from 'querystring';
 
-const authObject = {
-  username: 'admin@hel.fi',
-  organization: 'dc7d39db-b35a-4612-a921-1b7b24b0baa3',
-  resource: 'tprek:0010',
-  created_at: '2020-10-14',
-  valid_until: '2020-10-14',
-  signature: '50A2A31D50F776D23A87F06708934C43CC58B3E44F851018675266393A457828',
+// TODO: later on username will come from env variables.
+const parseUserName = (queryParams: string): string | undefined => {
+  const userNameParameters: string | string[] = querystring.parse(queryParams)
+    ?.username;
+  const userNameStr: string =
+    typeof userNameParameters === 'string'
+      ? userNameParameters
+      : userNameParameters[0];
+
+  return userNameStr ? decodeURIComponent(userNameStr) : undefined;
 };
 
 describe('Unauthenticated user', () => {
@@ -28,6 +32,8 @@ describe('Authenticated user', () => {
 
   it('Has username in header', () => {
     cy.get('header').first().should('not.contain', 'Kirjaudu');
-    cy.get('header').first().should('contain', authObject.username);
+    cy.get('header')
+      .first()
+      .should('contain', parseUserName(Cypress.env('AUTH_QUERY_PARAMETERS')));
   });
 });
