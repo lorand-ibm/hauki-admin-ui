e11708135981e47b900bacae464b73e966f74c17
Mika Nevalainen <mn.nevalainen@gmail.com>
2020-11-04 16:34:52 +0200

    Rename env variables

--- scripts/generate-auth-params.js
+++ scripts/generate-auth-params.js
@@ -2,16 +2,16 @@
 const crypto = require('crypto');
 
 const key =
-  process.env.APP_KEY ||
+  process.env.HAUKI_KEY ||
   'd94c278085e49e807b67a58b8fcb92239b3fe881252f1938e3e0ca09611684b3790df3a22c08a32959309fdfcf074b4204c2f84a1ef03025621c4cf23a5521f24299ba857956a1fcf1b61715cb8154baeedba4e0409e636da9e91d7a618a4403e748c3a2';
 
-const username = process.env.APP_USER || 'admin@hel.fi';
-const resource = process.env.APP_RESOURCE;
-const organization = process.env.APP_ORGANISATION;
+const username = process.env.HAUKI_USER || 'admin@hel.fi';
+const resource = process.env.HAUKI_RESOURCE;
+const organization = process.env.HAUKI_ORGANISATION;
 const now = new Date();
 const createdAt = now.toJSON();
 const validUntil =
-  process.env.APP_VALID_UNTIL ||
+  process.env.HAUKI_VALID_UNTIL ||
   new Date(now.setDate(now.getDate() + 7)).toJSON();
 
 const signatureStr = `${username}${createdAt}${validUntil}${resource || ''}${
