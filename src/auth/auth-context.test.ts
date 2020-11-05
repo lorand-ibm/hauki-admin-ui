c99638212872c0bab762b91c4161f75d719a8b1a
Mika Nevalainen <mn.nevalainen@gmail.com>
2020-11-05 16:15:47 +0200

    Fix tests

--- src/auth/auth-context.test.ts
+++ src/auth/auth-context.test.ts
@@ -7,20 +7,22 @@ describe('Auth Context', () => {
     it('parses auth parameters from query string', () => {
       expect(
         parseAuthParams(
-          '?username=admin@hel.fi&signature=1234567&valid_until=2020-10-02&created_at=2020-10-01'
+          '?username=admin@hel.fi&signature=1234567&valid_until=2020-11-05T16%3A20%3A00&created_at=2020-11-05T16%3A10%3A00&organization=12345&resource=tprek%3A8215'
         )
       ).toEqual({
         username: 'admin@hel.fi',
         signature: '1234567',
-        valid_until: '2020-10-02',
-        created_at: '2020-10-01',
+        valid_until: '2020-11-05T16:20:00',
+        created_at: '2020-11-05T16:10:00',
+        organization: '12345',
+        resource: 'tprek:8215',
       });
     });
 
     it('returns undefined when a required parameter is missing', () => {
       expect(
         parseAuthParams(
-          '?username=admin@hel.fi&valid_until=2020-10-02&created_at=2020-10-01'
+          '?username=admin@hel.fi&signature=1234567&valid_until=2020-11-05T16%3A20%3A00&created_at=2020-11-05T16%3A10%3A00'
         )
       ).toEqual(undefined);
     });
