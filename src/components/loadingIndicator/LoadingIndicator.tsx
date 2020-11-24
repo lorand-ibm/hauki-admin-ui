21cd3e8d61e4e140458429326118e12bea666784
Mika Nevalainen <mn.nevalainen@gmail.com>
2020-11-24 17:30:39 +0200

    Use hds-react loading-spinner

--- src/components/loadingIndicator/LoadingIndicator.tsx
+++ src/components/loadingIndicator/LoadingIndicator.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
+import { LoadingSpinner } from 'hds-react';
 import './LoadingIndicator.scss';
 
 export default function LoadingIndicator({
   text,
+  readyText,
 }: {
   text: string;
+  readyText: string;
 }): JSX.Element {
   return (
-    <div className="loading-indicator">
-      <div className="hds-loading-spinner hds-loading-spinner--small">
-        <div />
-        <div />
-        <div />
-      </div>
+    <div className="loading-indicator" data-test="loading-indicator">
+      <LoadingSpinner
+        small
+        loadingText={text}
+        loadingFinishedText={readyText}
+      />
       <span className="loading-indicator-text">{text}</span>
     </div>
   );
