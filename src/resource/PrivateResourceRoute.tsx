cf5f4ed9f7392322ed69d3ac20aabaabd5c0b141
Mika Nevalainen <mn.nevalainen@gmail.com>
2020-11-25 15:04:14 +0200

    Refactor route-component to be more independent - previously required a surrounding Switch-component

--- src/resource/PrivateResourceRoute.tsx
+++ src/resource/PrivateResourceRoute.tsx
@@ -8,6 +8,56 @@ import {
 import { AuthContextProps, useAuth } from '../auth/auth-context';
 import api from '../common/utils/api/api';
 
+const PermissionResolver = ({
+  id,
+  children,
+}: {
+  id?: string;
+  children?: ReactNode;
+}): JSX.Element => {
+  const { authTokens, clearAuth }: Partial<AuthContextProps> = useAuth();
+  const isAuthenticated = !!authTokens;
+  const [isLoading, setLoading] = useState<boolean>(true);
+  const [isAuthorized, setIsAuthorized] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (!id || !isAuthenticated) {
+      setLoading(false);
+    } else {
+      api
+        .testPostPermission(id)
+        .then((hasPermission: boolean) => {
+          setIsAuthorized(hasPermission);
+          setLoading(false);
+        })
+        .catch(() => {
+          if (clearAuth) {
+            clearAuth();
+          }
+          setLoading(false);
+        });
+    }
+  }, [clearAuth, id, isAuthenticated]);
+
+  if (isLoading) {
+    return (
+      <div>
+        <h1>Sivua alustetaan..</h1>
+      </div>
+    );
+  }
+
+  if (!isAuthenticated) {
+    return <Redirect to="/" />;
+  }
+
+  if (!isAuthorized) {
+    return <Redirect to="/unauthorized" />;
+  }
+
+  return <>{children}</>;
+};
+
 type ComputedMatch = {
   params: {
     id?: string;
@@ -20,35 +70,14 @@ interface PrivateRouteProps extends RouteProps {
 }
 
 export default function (routeProps: PrivateRouteProps): JSX.Element {
-  const { render, id, ...rest } = routeProps;
-  const resourceId = rest.computedMatch?.params.id;
-  const { authTokens, clearAuth }: Partial<AuthContextProps> = useAuth();
-  const [isLoading, setLoading] = useState<boolean>(true);
-  const [isAuthorized, setIsAuthorized] = useState<boolean>(false);
-
-  useEffect(() => {
-    if (!resourceId || !authTokens) {
-      setLoading(false);
-    } else {
-      api
-        .testPostPermission(resourceId)
-        .then((hasPermission) => {
-          setIsAuthorized(hasPermission);
-          setLoading(false);
-        })
-        .catch(() => {
-          if (clearAuth) {
-            clearAuth();
-          }
-          setLoading(false);
-        });
-    }
-  }, [id, authTokens, clearAuth, resourceId]);
+  const { render, ...rest } = routeProps;
 
   return (
     <Route
       {...rest}
-      render={(props: RouteComponentProps): JSX.Element | ReactNode => {
+      render={(
+        props: RouteComponentProps<{ id: string }>
+      ): JSX.Element | ReactNode => {
         if (!render) {
           // eslint-disable-next-line no-console
           console.error(
@@ -57,23 +86,13 @@ export default function (routeProps: PrivateRouteProps): JSX.Element {
           return <Redirect to="/" />;
         }
 
-        if (isLoading) {
-          return (
-            <div>
-              <h1>Sivua alustetaan..</h1>
-            </div>
-          );
-        }
-
-        if (!authTokens) {
-          return <Redirect to="/" />;
-        }
-
-        if (!isAuthorized) {
-          return <Redirect to="/unauthorized" />;
-        }
-
-        return render(props);
+        return (
+          <PermissionResolver
+            key={props.location.key}
+            id={props.match.params.id}>
+            {render(props)}
+          </PermissionResolver>
+        );
       }}
     />
   );
