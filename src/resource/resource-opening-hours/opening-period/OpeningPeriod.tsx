458f3fa4acfe15338c4b6c772d6d4f12d8d97b61
Mika Nevalainen <mn.nevalainen@gmail.com>
2020-12-02 16:30:57 +0200

    Add edit-link-component to date-period list item

--- src/resource/resource-opening-hours/opening-period/OpeningPeriod.tsx
+++ src/resource/resource-opening-hours/opening-period/OpeningPeriod.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { IconPenLine } from 'hds-react';
 import { DatePeriod } from '../../../common/lib/types';
 import { formatDateRange } from '../../../common/utils/date-time/format';
 import './OpeningPeriod.scss';
 
 export default function OpeningPeriod({
+  resourceId,
   datePeriod,
 }: {
+  resourceId: string;
   datePeriod: DatePeriod;
 }): JSX.Element {
+  const name = datePeriod.name?.fi;
+
   return (
     <div className="opening-period">
       <div className="opening-period-row">
@@ -21,6 +27,14 @@ export default function OpeningPeriod({
         </div>
         <div className="opening-period-title">
           <h4>{datePeriod.name?.fi}</h4>
+          <Link
+            className="opening-period-edit-link"
+            to={`/resource/${resourceId}/period/${datePeriod.id}`}>
+            <IconPenLine aria-hidden="true" />
+            <span className="sr-only">{`Muokkaa ${
+              name || 'nimettömän'
+            } aukiolojakson tietoja`}</span>
+          </Link>
         </div>
       </div>
     </div>
