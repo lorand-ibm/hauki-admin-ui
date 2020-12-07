b0c36b9e19630b244b89264f6e5afeb2268aaac7
Mika Nevalainen <mn.nevalainen@gmail.com>
2020-12-07 21:23:23 +0200

    Use language-select in data-period lists

--- src/resource/resource-opening-hours/opening-period/OpeningPeriod.tsx
+++ src/resource/resource-opening-hours/opening-period/OpeningPeriod.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { IconPenLine } from 'hds-react';
-import { DatePeriod } from '../../../common/lib/types';
+import { DatePeriod, Language } from '../../../common/lib/types';
 import { formatDateRange } from '../../../common/utils/date-time/format';
+import { getMissingText } from '../../../components/language-select/LanguageSelect';
 import './OpeningPeriod.scss';
 
 export default function OpeningPeriod({
   resourceId,
   datePeriod,
+  language,
 }: {
   resourceId: string;
   datePeriod: DatePeriod;
+  language: Language;
 }): JSX.Element {
-  const name = datePeriod.name?.fi;
+  const name = datePeriod.name[language];
 
   return (
     <div className="opening-period">
@@ -26,7 +29,13 @@ export default function OpeningPeriod({
           </div>
         </div>
         <div className="opening-period-title opening-period-row-column">
-          <h4>{datePeriod.name?.fi}</h4>
+          {name ? (
+            <h4>{name}</h4>
+          ) : (
+            <h4 className="text-danger">
+              {getMissingText({ language, label: 'nimi' })}
+            </h4>
+          )}
         </div>
         <div className="opening-period-actions opening-period-row-column">
           <Link
