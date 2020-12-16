a09ffed87d48ad991e591f97e5886a9f95723740
Mika Nevalainen <mn.nevalainen@gmail.com>
2020-12-16 17:33:46 +0200

    Unify formatDateRange implementation, add test

--- src/common/utils/date-time/format.ts
+++ src/common/utils/date-time/format.ts
@@ -1,8 +1,12 @@
 import parse from 'date-fns/parse';
 import format from 'date-fns/format';
 
+export const dateApiFormat = 'yyyy-MM-dd';
+export const dateFormFormat = 'dd.MM.yyyy';
+export const datetimeFormFormat = `${dateFormFormat} HH:mm`;
+
 export const formatDate = (date: string): string =>
-  new Date(date).toLocaleDateString();
+  format(new Date(date), dateFormFormat);
 
 export const formatDateRange = ({
   startDate,
@@ -18,11 +22,9 @@ export const formatDateRange = ({
   if (!endDate) {
     return formatDate(startDate);
   }
+
   return `${formatDate(startDate)} - ${formatDate(endDate)}`;
 };
 
-export const dateApiFormat = 'yyyy-MM-dd';
-export const dateFormFormat = 'dd.MM.yyyy';
-export const datetimeFormFormat = `${dateFormFormat} HH:mm`;
 export const transformToApiFormat = (formDate: string): string =>
   format(parse(formDate, dateFormFormat, new Date()), dateApiFormat);
