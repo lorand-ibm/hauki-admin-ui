111a0be8c6da9ede3a5693aa991500736c25cc28
Mika Nevalainen <mn.nevalainen@gmail.com>
2020-12-16 13:47:38 +0200

    Use new opening-period-form in edit date-period page

--- src/opening-period/page/EditOpeningPeriodPage.tsx
+++ src/opening-period/page/EditOpeningPeriodPage.tsx
@@ -1,60 +1,18 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import { Control, Controller, FieldValues, useForm } from 'react-hook-form';
-import { Button, LoadingSpinner, Notification } from 'hds-react';
-import { useHistory } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Notification } from 'hds-react';
 import api from '../../common/utils/api/api';
 import {
   DatePeriod,
-  Language,
+  DatePeriodOptions,
   Resource,
-  ResourceState,
 } from '../../common/lib/types';
-import { transformToApiFormat } from '../../common/utils/date-time/format';
-import Datepicker from '../../components/datepicker/Datepicker';
-import { ErrorToast, SuccessToast } from '../../components/notification/Toast';
 import {
   ResourceInfo,
   ResourceTitle,
   ResourceAddress,
 } from '../../resource/page/ResourcePage';
+import OpeningPeriodForm from '../form/OpeningPeriodForm';
 import './EditOpeningPeriodPage.scss';
-
-type FormData = {
-  openingPeriodTitle: string;
-  openingPeriodOptionalDescription: string;
-  openingPeriodBeginDate: string;
-  openingPeriodEndDate: string;
-};
-
-type SubmitStatus = 'init' | 'succeeded' | 'error';
-
-const FormDatePicker = ({
-  name,
-  label,
-  control,
-  register,
-}: {
-  name: string;
-  label: string;
-  control: Control<FormData & FieldValues>;
-  register: () => void;
-}): JSX.Element => (
-  <Controller
-    control={control}
-    name={name}
-    render={({ value, onChange }): JSX.Element => (
-      <Datepicker
-        id={name}
-        dataTest={name}
-        labelText={label}
-        onChange={onChange}
-        value={new Date(value)}
-        registerFn={register}
-        required
-      />
-    )}
-  />
-);
 
 export default function EditOpeningPeriodPage({
   resourceId,
@@ -64,84 +22,45 @@ export default function EditOpeningPeriodPage({
   datePeriodId: string;
 }): JSX.Element {
   const id = parseInt(datePeriodId, 10);
-  const selectedLanguage: Language = Language.FI;
   const [resource, setResource] = useState<Resource>();
-  const [hasDataLoadingError, setHasDataLoadingError] = useState<
-    Error | undefined
-  >(undefined);
-  const { reset, register, handleSubmit, control, errors } = useForm<
-    FormData
+  const [datePeriod, setDatePeriod] = useState<DatePeriod>();
+  const [datePeriodOptions, setDatePeriodOptions] = useState<
+    DatePeriodOptions
   >();
-  const history = useHistory();
   const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [isSaving, setIsSaving] = useState<boolean>(false);
-  const [submitStatus, setSubmitStatus] = useState<SubmitStatus>('init');
-  const resetForm = useCallback(
-    (datePeriod: DatePeriod): void =>
-      reset({
-        openingPeriodTitle: datePeriod.name[selectedLanguage] || '',
-        openingPeriodOptionalDescription:
-          datePeriod.description[selectedLanguage] || '',
-        openingPeriodBeginDate: datePeriod.start_date || undefined,
-        openingPeriodEndDate: datePeriod.end_date || undefined,
-      }),
-    [reset, selectedLanguage]
+  const [hasDataLoadingError, setHasDataLoadingError] = useState<boolean>(
+    false
   );
 
-  const onSubmit = async (data: FormData): Promise<void> => {
-    try {
-      if (!resource || !data) {
-        throw new Error('This should never happen, but typescript');
-      }
-
-      setIsSaving(true);
-      const updatedPeriod = await api.putDatePeriod({
-        resource: resource.id,
-        id,
-        name: {
-          fi: data.openingPeriodTitle,
-          sv: null,
-          en: null,
-        },
-        description: {
-          fi: data.openingPeriodOptionalDescription,
-          sv: null,
-          en: null,
-        },
-        start_date: transformToApiFormat(data.openingPeriodBeginDate),
-        end_date: transformToApiFormat(data.openingPeriodEndDate),
-        resource_state: ResourceState.OPEN,
-        override: false,
-        time_span_groups: [],
-      });
-      resetForm(updatedPeriod);
-      setIsSaving(false);
-      setSubmitStatus('succeeded');
-    } catch (err) {
-      setIsSaving(false);
-      setSubmitStatus('error');
-      throw err;
-    }
-  };
+  const submitFn = (updatedDatePeriod: DatePeriod): Promise<DatePeriod> =>
+    api.putDatePeriod(updatedDatePeriod);
 
   useEffect((): void => {
     const fetchData = async (): Promise<void> => {
       try {
-        const [apiResource, datePeriod] = await Promise.all([
+        const [
+          apiResource,
+          apiDatePeriod,
+          apiDatePeriodOptions,
+        ] = await Promise.all([
           api.getResource(resourceId),
           api.getDatePeriod(id),
+          api.getDatePeriodFormOptions(),
         ]);
         setResource(apiResource);
-        resetForm(datePeriod);
+        setDatePeriod(apiDatePeriod);
+        setDatePeriodOptions(apiDatePeriodOptions);
         setIsLoading(false);
       } catch (e) {
-        setHasDataLoadingError(e);
+        setHasDataLoadingError(true);
         setIsLoading(false);
+        // eslint-disable-next-line no-console
+        console.error('Edit dateperiod - data initialization error:', e);
       }
     };
 
     fetchData();
-  }, [id, resetForm, resourceId]);
+  }, [id, resourceId]);
 
   if (hasDataLoadingError) {
     return (
@@ -166,21 +85,6 @@ export default function EditOpeningPeriodPage({
 
   return (
     <>
-      {submitStatus === 'succeeded' && (
-        <SuccessToast
-          dataTestId="opening-period-edited-successfully-notification"
-          label="Aukiolojakson muutokset tallennettu onnistuneesti"
-          text="Aukiolojakso muutokset tallennettu onnistuneesti"
-          onClose={(): void => setSubmitStatus('init')}
-        />
-      )}
-      {submitStatus === 'error' && (
-        <ErrorToast
-          label="Aukiolojakson muokkaaminen epäonnistui"
-          text="Aukiolojakson muokkaaminen epäonnistui. Yritä myöhemmin uudestaan."
-          onClose={(): void => setSubmitStatus('init')}
-        />
-      )}
       <ResourceInfo>
         <ResourceTitle resource={resource}>
           <h2 className="opening-period-page-title">
@@ -189,89 +93,24 @@ export default function EditOpeningPeriodPage({
         </ResourceTitle>
         <ResourceAddress resource={resource} />
       </ResourceInfo>
-      <form
-        id="opening-period-form"
-        data-test="opening-period-form"
-        className="opening-period-form"
-        onSubmit={handleSubmit(onSubmit)}>
-        <div className="edit-form-content-container">
-          <section className="opening-period-form-section">
-            <h3 className="opening-period-section-title">Jakson kuvaus</h3>
-            <label htmlFor="openingPeriodTitle">Aukiolojakson otsikko *</label>
-            <input
-              className="opening-period-title-input"
-              type="text"
-              name="openingPeriodTitle"
-              data-test="openingPeriodTitle"
-              id="openingPeriodTitle"
-              aria-invalid={errors.openingPeriodTitle ? 'true' : 'false'}
-              ref={register({ required: true, maxLength: 100 })}
-            />
-            {errors.openingPeriodTitle &&
-              errors.openingPeriodTitle.type === 'required' && (
-                <span role="alert">Aukiolojakson otsikko on pakollinen</span>
-              )}
-
-            <label htmlFor="openingPeriodOptionalDescription">
-              Jakson valinnainen kuvaus
-            </label>
-            <textarea
-              cols={90}
-              className="opening-period-optional-description-input"
-              id="openingPeriodOptionalDescription"
-              name="openingPeriodOptionalDescription"
-              ref={register({ maxLength: 255 })}
-            />
-          </section>
-          <section className="opening-period-form-section">
-            <h3 className="opening-period-section-title">Ajanjakso</h3>
-            <div className="opening-period-form-dates">
-              <FormDatePicker
-                name="openingPeriodBeginDate"
-                label="Alkaa"
-                control={control}
-                register={register}
-              />
-              <p className="dash-between-begin-and-end-date">—</p>
-              <FormDatePicker
-                name="openingPeriodEndDate"
-                label="Päättyy"
-                control={control}
-                register={register}
-              />
-            </div>
-          </section>
-        </div>
-        <div className="opening-period-final-action-row-container">
-          <Button
-            data-test="opening-period-save-button"
-            className="opening-period-final-action-button publish-opening-period-button period-save-button"
-            type="submit"
-            form="opening-period-form"
-            disabled={isSaving}>
-            {isSaving && (
-              <div className="period-save-button-spinner">
-                <LoadingSpinner
-                  small
-                  loadingText="Tallentaa aukiolojakson tietoja"
-                  loadingFinishedText="Aukiolojakson tiedot tallennettu"
-                  theme={{
-                    '--spinner-color':
-                      'var(--period-save-button-spinner-color)',
-                  }}
-                />
-              </div>
-            )}
-            Julkaise
-          </Button>
-          <Button
-            onClick={(): void => history.push(`/resource/${resourceId}`)}
-            className="opening-period-final-action-button cancel-opening-period-button"
-            variant="secondary">
-            Peruuta ja palaa
-          </Button>
-        </div>
-      </form>
+      {resource && datePeriod && datePeriodOptions && (
+        <OpeningPeriodForm
+          formId="edit-opening-period-form"
+          datePeriod={datePeriod}
+          resourceId={resource.id}
+          datePeriodOptions={datePeriodOptions}
+          submitFn={submitFn}
+          successTexts={{
+            label: 'Aukiolojakson muutokset tallennettu onnistuneesti.',
+            description: 'Aukiolojakson muutokset tallennettu onnistuneesti.',
+          }}
+          errorTexts={{
+            label: 'Aukiolojakson muokkaus epäonnistui',
+            description:
+              'Aukiolojakson muokkaus epäonnistui. Yritä myöhemmin uudestaan.',
+          }}
+        />
+      )}
     </>
   );
 }
