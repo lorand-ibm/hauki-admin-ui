1c68a3ecb6228325a30719ba867b058e3a1eb2f8
Mika Nevalainen <mn.nevalainen@gmail.com>
2020-12-18 16:59:52 +0200

    Add tests for EditOpeningPeriodPage

---
+++ src/opening-period/page/EditOpeningPeriodPage.test.tsx
@@ -0,0 +1,332 @@
+import React from 'react';
+import { act } from 'react-dom/test-utils';
+import { render, screen } from '@testing-library/react';
+import {
+  DatePeriod,
+  DatePeriodOptions,
+  Resource,
+  ResourceState,
+} from '../../common/lib/types';
+import api from '../../common/utils/api/api';
+import EditOpeningPeriodPage from './EditOpeningPeriodPage';
+
+const testDatePeriodOptions: DatePeriodOptions = {
+  actions: {
+    POST: {
+      resource_state: {
+        choices: [
+          {
+            value: 'open',
+            display_name: 'Open',
+          },
+          {
+            value: 'closed',
+            display_name: {
+              fi: 'Suljettu',
+              sv: null,
+              en: null,
+            },
+          },
+          {
+            value: 'self_service',
+            display_name: 'Self service',
+          },
+        ],
+      },
+    },
+  },
+};
+
+const testResource: Resource = {
+  id: 1186,
+  name: {
+    fi: 'Test resource name in finnish',
+    sv: 'Test resource name in swedish',
+    en: 'Test resource name in english',
+  },
+  address: {
+    fi: 'Test address in finnish',
+    sv: 'Test address in swedish',
+    en: 'Test address in english',
+  },
+  description: {
+    fi: 'Test description in finnish',
+    sv: 'Test description in swedish',
+    en: 'Test description in english',
+  },
+  extra_data: {
+    citizen_url: 'kansalaisen puolen url',
+    admin_url: 'admin puolen url',
+  },
+};
+
+const testDatePeriod: DatePeriod = {
+  id: 1,
+  created: '2020-11-20',
+  modified: '2020-11-20',
+  is_removed: false,
+  name: { fi: 'test opening period', sv: '', en: '' },
+  description: { fi: 'test opening period description', sv: '', en: '' },
+  start_date: '2020-12-18',
+  end_date: '2021-01-18',
+  resource_state: ResourceState.OPEN,
+  override: false,
+  resource: 1,
+  time_span_groups: [
+    {
+      id: 2,
+      period: 1,
+      rules: [],
+      time_spans: [
+        {
+          created: '2020-12-18T10:00:00.000000+02:00',
+          description: {
+            fi: 'Viikonloppuna ovet menevät kiinni tuntia aiemmin',
+            sv: null,
+            en: null,
+          },
+          end_time: '17:00:00',
+          full_day: false,
+          group: 1225,
+          id: 2637,
+          modified: '2020-12-18T10:00:00.000000+02:00',
+          name: {
+            fi: null,
+            sv: null,
+            en: null,
+          },
+          resource_state: ResourceState.SELF_SERVICE,
+          start_time: '12:00:00',
+          weekdays: [6, 7],
+        },
+        {
+          created: '2020-12-18T10:00:00.000000+02:00',
+          description: {
+            fi: 'Arkena ovet menevät kiinni puolta tuntia aiemmin',
+            sv: null,
+            en: null,
+          },
+          end_time: '18:00:00',
+          full_day: false,
+          group: 1225,
+          id: 2636,
+          modified: '2020-12-18T10:00:00.000000+02:00',
+          name: {
+            fi: null,
+            sv: null,
+            en: null,
+          },
+          resource_state: ResourceState.OPEN,
+          start_time: '10:00:00',
+          weekdays: [1, 2, 3, 4, 5],
+        },
+      ],
+    },
+  ],
+};
+
+describe(`<EditNewOpeningPeriodPage />`, () => {
+  beforeEach(() => {
+    jest
+      .spyOn(api, 'getResource')
+      .mockImplementation(() => Promise.resolve(testResource));
+
+    jest
+      .spyOn(api, 'getDatePeriodFormOptions')
+      .mockImplementation(() => Promise.resolve(testDatePeriodOptions));
+
+    jest
+      .spyOn(api, 'getDatePeriod')
+      .mockImplementation(() => Promise.resolve(testDatePeriod));
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should render basic date-period data', async () => {
+    let container: HTMLElement;
+
+    await act(async () => {
+      container = render(
+        <EditOpeningPeriodPage resourceId="tprek:8100" datePeriodId="1" />
+      ).container;
+
+      if (!container) {
+        throw new Error(
+          'Something went wrong in rendering of CreateNewOpeningPeriodPage'
+        );
+      }
+    });
+
+    await act(async () => {
+      // Check info data
+      expect(
+        container.querySelector('[data-test="openingPeriodTitle"]')
+      ).toHaveValue(testDatePeriod.name.fi);
+
+      expect(
+        container.querySelector('#openingPeriodOptionalDescription')
+      ).toHaveValue(testDatePeriod.description.fi);
+
+      expect(container.querySelector('#openingPeriodBeginDate')).toHaveValue(
+        '18.12.2020'
+      );
+      expect(container.querySelector('#openingPeriodEndDate')).toHaveValue(
+        '18.01.2021'
+      );
+    });
+  });
+
+  it('should render first the time-span with weekdays in the beginning of the week', async () => {
+    let container: HTMLElement;
+
+    await act(async () => {
+      container = render(
+        <EditOpeningPeriodPage resourceId="tprek:8100" datePeriodId="1" />
+      ).container;
+
+      if (!container) {
+        throw new Error(
+          'Something went wrong in rendering of CreateNewOpeningPeriodPage'
+        );
+      }
+    });
+
+    await act(async () => {
+      expect(
+        container.querySelector('[data-test="weekdays-monday-0-checkbox"]')
+      ).toBeChecked();
+
+      expect(
+        container.querySelector('[data-test="weekdays-tuesday-0-checkbox"]')
+      ).toBeChecked();
+
+      expect(
+        container.querySelector('[data-test="weekdays-wednesday-0-checkbox"]')
+      ).toBeChecked();
+
+      expect(
+        container.querySelector('[data-test="weekdays-thursday-0-checkbox"]')
+      ).toBeChecked();
+
+      expect(
+        container.querySelector('[data-test="weekdays-friday-0-checkbox"]')
+      ).toBeChecked();
+
+      expect(
+        container.querySelector('[data-test="weekdays-saturday-0-checkbox"]')
+      ).not.toBeChecked();
+
+      expect(
+        container.querySelector('[data-test="weekdays-sunday-0-checkbox"]')
+      ).not.toBeChecked();
+
+      expect(
+        container.querySelector('#time-span-state-id-0-toggle-button')
+      ).toHaveTextContent('Open');
+
+      expect(container.querySelector('#time-span-0-description')).toHaveValue(
+        'Arkena ovet menevät kiinni puolta tuntia aiemmin'
+      );
+    });
+  });
+
+  it('should render last the time-span with weekdays in the end of the week', async () => {
+    let container: HTMLElement;
+
+    await act(async () => {
+      container = render(
+        <EditOpeningPeriodPage resourceId="tprek:8100" datePeriodId="1" />
+      ).container;
+
+      if (!container) {
+        throw new Error(
+          'Something went wrong in rendering of CreateNewOpeningPeriodPage'
+        );
+      }
+    });
+
+    await act(async () => {
+      expect(
+        container.querySelector('[data-test="weekdays-monday-1-checkbox"]')
+      ).not.toBeChecked();
+
+      expect(
+        container.querySelector('[data-test="weekdays-tuesday-1-checkbox"]')
+      ).not.toBeChecked();
+
+      expect(
+        container.querySelector('[data-test="weekdays-wednesday-1-checkbox"]')
+      ).not.toBeChecked();
+
+      expect(
+        container.querySelector('[data-test="weekdays-thursday-1-checkbox"]')
+      ).not.toBeChecked();
+
+      expect(
+        container.querySelector('[data-test="weekdays-friday-1-checkbox"]')
+      ).not.toBeChecked();
+
+      expect(
+        container.querySelector('[data-test="weekdays-saturday-1-checkbox"]')
+      ).toBeChecked();
+
+      expect(
+        container.querySelector('[data-test="weekdays-sunday-1-checkbox"]')
+      ).toBeChecked();
+
+      expect(
+        container.querySelector('#time-span-state-id-1-toggle-button')
+      ).toHaveTextContent('Self service');
+
+      expect(container.querySelector('#time-span-1-description')).toHaveValue(
+        'Viikonloppuna ovet menevät kiinni tuntia aiemmin'
+      );
+    });
+  });
+
+  it('should show loading indicator while loading date period', async () => {
+    jest.spyOn(api, 'getDatePeriod').mockImplementation(() => {
+      // eslint-disable-next-line @typescript-eslint/no-empty-function
+      return new Promise(() => {});
+    });
+
+    render(<EditOpeningPeriodPage resourceId="tprek:8100" datePeriodId="1" />);
+
+    await act(async () => {
+      expect(await screen.getByRole('heading', { level: 1 })).toHaveTextContent(
+        'Aukiolojakson muokkaus'
+      );
+      expect(await screen.getByText('Sivua ladataan...')).toBeInTheDocument();
+    });
+  });
+
+  it('should show error notification if loading date period fails', async () => {
+    const error: Error = new Error('Failed to load date period form options');
+    const errorConsoleSpy = jest
+      .spyOn(global.console, 'error')
+      .mockImplementationOnce((cb) => cb);
+
+    jest
+      .spyOn(api, 'getDatePeriod')
+      .mockImplementation(() =>
+        Promise.reject(new Error('Failed to load date period form options'))
+      );
+
+    render(<EditOpeningPeriodPage resourceId="tprek:8100" datePeriodId="1" />);
+
+    await act(async () => {
+      const errorHeading = await screen.getByRole('heading', { level: 1 });
+      const notificationText = await screen.findByTestId(
+        'error-retrieving-resource-info'
+      );
+      expect(errorConsoleSpy).toHaveBeenCalledWith(
+        'Edit date-period - data initialization error:',
+        error
+      );
+      expect(errorHeading).toHaveTextContent('Virhe');
+      expect(notificationText).toBeInTheDocument();
+    });
+  });
+});
