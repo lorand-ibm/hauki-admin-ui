52a0f06560b1bc0ca5d798d87043c91d623c8303
Mika Nevalainen <mn.nevalainen@gmail.com>
2021-01-15 17:37:24 +0200

    Add components to list rules

---
+++ src/opening-period/rule/Rules.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { ArrayField, Control, useFieldArray } from 'react-hook-form';
+import { SecondaryButton } from '../../components/button/Button';
+import { GroupRuleFormFormat, UiRuleConfig } from '../../common/lib/types';
+import Rule from './Rule';
+
+export default function Rules({
+  namePrefix,
+  control,
+  ruleConfig,
+  register,
+  setValue,
+}: {
+  namePrefix: string;
+  control: Control<Record<string, any>>;
+  ruleConfig: UiRuleConfig;
+  register: Function;
+  setValue: Function;
+}): JSX.Element {
+  const ruleNamePrefix = `${namePrefix}.rules`;
+  const { fields, remove, append } = useFieldArray({
+    control,
+    name: ruleNamePrefix,
+    keyName: 'rulesUiId',
+  });
+
+  return (
+    <>
+      <h3 className="opening-period-section-title">
+        Aukioloaikojen voimassaolo
+      </h3>
+      <ul
+        className="opening-period-field-list opening-period-rule-list form-group"
+        data-test="rule-list">
+        {fields.map(
+          (
+            rule: Partial<ArrayField<Record<string, GroupRuleFormFormat>>>,
+            index: number
+          ) => (
+            <li
+              className="opening-period-field-list-item opening-period-rule-list-item"
+              key={`rule-${rule.id || index}`}
+              data-test={`rule-list-item-${rule.id || index}`}>
+              <Rule
+                namePrefix={ruleNamePrefix}
+                index={index}
+                rule={rule}
+                control={control}
+                setValue={setValue}
+                remove={remove}
+                register={register}
+                ruleConfig={ruleConfig}
+              />
+            </li>
+          )
+        )}
+      </ul>
+      <div className="form-group">
+        <SecondaryButton
+          dataTest="add-new-rule-button"
+          onClick={(): void => append({})}>
+          + Lisää aukioloaikojen voimassaolosääntö
+        </SecondaryButton>
+      </div>
+    </>
+  );
+}
