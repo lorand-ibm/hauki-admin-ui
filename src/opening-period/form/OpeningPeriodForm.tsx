768b7af1c42b82cdd5f3d1dcf27078601a6c2bad
Mika Nevalainen <mn.nevalainen@gmail.com>
2021-02-18 17:34:07 +0200

    Align clear-button

--- src/opening-period/form/OpeningPeriodForm.tsx
+++ src/opening-period/form/OpeningPeriodForm.tsx
@@ -321,6 +321,7 @@ export default function OpeningPeriodForm({
                   options={resourceStateConfig.options}
                 />
                 <SupplementaryButton
+                  className="opening-period-clear-state-button"
                   dataTest="clear-resource-state"
                   onClick={(): void => {
                     clearResourceState();
