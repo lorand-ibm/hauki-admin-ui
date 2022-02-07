3742c0975c7d9ccc3f313f04e80cdfce56f3a633
Mika Nevalainen <mn.nevalainen@gmail.com>
2022-02-07 17:28:19 +0200

    Fix test

--- src/components/navigation/HaukiNavigation.test.tsx
+++ src/components/navigation/HaukiNavigation.test.tsx
@@ -9,7 +9,7 @@ describe('<HaukiNavigation>', () => {
   test('Should show close button if app window has a referrer', () => {
     const closeAppWindow = jest.fn();
     const { container } = render(
-      <AppContext.Provider value={{ hasReferrer: true, closeAppWindow }}>
+      <AppContext.Provider value={{ hasOpenerWindow: true, closeAppWindow }}>
         <HaukiNavigation />
       </AppContext.Provider>
     );
