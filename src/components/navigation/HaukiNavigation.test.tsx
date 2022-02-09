bd18f70e80f16eada38ede6cc1f1ac9aeca22bae
Mika Nevalainen <mn.nevalainen@gmail.com>
2022-02-09 19:13:54 +0200

    Fix navigation test after merge

--- src/components/navigation/HaukiNavigation.test.tsx
+++ src/components/navigation/HaukiNavigation.test.tsx
@@ -1,22 +1,47 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { getElementOrThrow } from '../../../test/test-utils';
+import api from '../../common/utils/api/api';
 import { AppContext } from '../../App-context';
+import { AuthContext } from '../../auth/auth-context';
 import HaukiNavigation from './HaukiNavigation';
 
 describe('<HaukiNavigation>', () => {
-  test('Should show close button if app window has a referrer', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('Should logout and close window when user clicks close button', async () => {
     const closeAppWindow = jest.fn();
+    jest.mock('react-router-dom', () => ({
+      ...jest.requireActual('react-router-dom'),
+      useHistory: () => ({
+        push: jest.fn(),
+      }),
+    }));
+
+    jest
+      .spyOn(api, 'invalidateAuth')
+      .mockImplementation(() => Promise.resolve(true));
+
     const { container } = render(
-      <AppContext.Provider value={{ hasOpenerWindow: true, closeAppWindow }}>
-        <HaukiNavigation />
-      </AppContext.Provider>
+      <Router>
+        <AppContext.Provider value={{ hasOpenerWindow: true, closeAppWindow }}>
+          <AuthContext.Provider
+            value={{ authTokens: { name: 'tester' }, clearAuth: jest.fn() }}>
+            <HaukiNavigation />
+          </AuthContext.Provider>
+        </AppContext.Provider>
+      </Router>
     );
     userEvent.click(
       getElementOrThrow(container, '[data-test="close-app-button"]')
     );
 
-    expect(closeAppWindow).toHaveBeenCalled();
+    await waitFor(async () => {
+      expect(closeAppWindow).toHaveBeenCalled();
+    });
   });
 });
