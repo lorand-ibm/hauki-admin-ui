abc186c498f36bbc016b114dc19ee9db508ba920
Mika Nevalainen <mn.nevalainen@gmail.com>
2022-04-14 16:42:58 +0300

    Add switch-buttons

--- src/opening-period/page/SimpleCreateOpeningHours.tsx
+++ src/opening-period/page/SimpleCreateOpeningHours.tsx
@@ -1,8 +1,60 @@
 import React, { useEffect, useState } from 'react';
+import { Button } from 'hds-react';
 import { Resource, UiDatePeriodConfig } from '../../common/lib/types';
 import api from '../../common/utils/api/api';
 
 import './SimpleCreateOpeningHours.scss';
+
+const SwitchButton = ({
+  isActive,
+  label,
+  onChange,
+}: {
+  isActive: boolean;
+  label: string;
+  onChange: () => void;
+}) => (
+  <Button
+    className={`switch-buttons-button ${
+      isActive ? 'switch-buttons-button--active' : ''
+    }`}
+    variant="secondary"
+    onClick={(): void => onChange()}>
+    {label}
+  </Button>
+);
+
+const SwitchButtons = ({
+  labels,
+  initialValue,
+  onChange,
+}: {
+  labels: { on: string; off: string };
+  initialValue: boolean;
+  onChange: (x: boolean) => void;
+}) => {
+  const [value, setValue] = useState<boolean>(initialValue);
+
+  useEffect(() => {
+    onChange(value);
+  }, [onChange, value]);
+
+  return (
+    <div className="switch-buttons">
+      <SwitchButton
+        isActive={value}
+        label={labels.on}
+        onChange={(): void => setValue(true)}
+      />
+      <span className="switch-buttons-divider">/</span>
+      <SwitchButton
+        isActive={!value}
+        label={labels.off}
+        onChange={(): void => setValue(false)}
+      />
+    </div>
+  );
+};
 
 export default function CreateNewOpeningPeriodPage({
   resourceId,
@@ -38,6 +90,11 @@ export default function CreateNewOpeningPeriodPage({
         {resource?.name?.fi}
       </h1>
       {resource && datePeriodConfig && <div />}
+      <SwitchButtons
+        labels={{ on: 'Kyllä', off: 'Ei' }}
+        initialValue={false}
+        onChange={console.log}
+      />
     </div>
   );
 }
